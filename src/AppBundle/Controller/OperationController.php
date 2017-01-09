7be5ba7a513b36bcf87943eace6aad42bc8606c2
Peter J. Kranz <peter@myref.net>
2017-01-09 22:37:09 +0100

    money input now possible with decimal places

--- src/AppBundle/Controller/OperationController.php
+++ src/AppBundle/Controller/OperationController.php
@@ -5,16 +5,19 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Operation;
 use AppBundle\Entity\Proceeding;
 use AppBundle\Entity\Split;
+use Money\Currencies\ISOCurrencies;
 use Money\Currency;
 use Money\Money;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Money\Parser\IntlMoneyParser;
 
 class OperationController extends Controller {
     
@@ -26,9 +29,18 @@ class OperationController extends Controller {
         $userRepository = $em->getRepository('AppBundle:User');
         $other = $userRepository->findOneById($userId);
         $me = $userRepository->findOneById($this->getUser()->getId());
-        $data = array('date' => new \DateTime('now'), 'currency' => 'EUR', 'amount' => '0.00');
-        $friends = $me->getFriends();
-        $friends->add($me);
+        $data = array('date' => new \DateTime('now'),
+                'currency' => 'EUR',
+                'amount' => '0.00'
+        );
+        $friends = $me->getFriends()->toArray();
+        $friends[] = $me;
+        
+        $currencies = array();
+        $isoCurrencies = new ISOCurrencies();
+        foreach ($isoCurrencies as $currency) {
+            $currencies[] = $currency->getCode();
+        }
         
         $builder = $this->createFormBuilder($data)
         ->add('date', DateType::class, array(
@@ -38,32 +50,41 @@ class OperationController extends Controller {
                         'class' => 'js-datepicker',
                 )
         ))
-        ->add('currency', TextType::class)
+        ->add('currency', ChoiceType::class,[
+                'choices' => $currencies,
+                'choice_label' => function($value, $key, $index) use (&$currencies) {
+                    return $value;
+                },
+                'group_by' => function($value, $key, $index) {
+                    return substr($value, 0, 1);  
+                },
+                'attr' => array(
+                    'class' => 'currencyinput'
+                )
+        ])
         ->add('amount', TextType::class, array('attr' => array('id' => 'moneyinput')))
-        ->add('sender', EntityType::class, array (
-                'class' => 'AppBundle:User',
-                'choices' => $this->getUser()->getFriends(),
+        ->add('sender', ChoiceType::class, array (
+                'choices' => $friends,
                 'expanded' => false,
-                'choice_label' => 'username',
+                'choice_label' => function($value, $key, $index) {
+                    return $value->getUsername();
+                },
                 'multiple' => false,
                 'attr' => array (
-                        'width' => '400',
                         'class' => 'js-example-basic-multiple'
                 ),
-                'data' => $me,
-                'em' => $em
+                'data' => $me
         ))
-        ->add('receiver', EntityType::class, array (
-                'class' => 'AppBundle:User',
-                'choices' => $this->getUser()->getFriends(),
-                'choice_label' => 'username',
+        ->add('receiver', ChoiceType::class, array (
+                'choices' => $friends,
+                'choice_label' => function($value, $key, $index) {
+                return $value->getUsername();
+                },
                 'multiple' => false,
                 'attr' => array (
-                        'width' => '400',
                         'class' => 'js-example-basic-multiple'
                 ),
-                'data' => $other,
-                'em' => $em
+                'data' => $other
         ))
         ->add('add', SubmitType::class, array (
                 'label' => 'Create Operation'
@@ -76,9 +97,15 @@ class OperationController extends Controller {
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
             
+
+            $numberFormatter = new \NumberFormatter('en_US', \NumberFormatter::CURRENCY);
+            $moneyParser = new IntlMoneyParser($numberFormatter, $isoCurrencies);
+            
+            $money = $moneyParser->parse($data['currency'].$data['amount']);
+            
+            
             $operation = new Operation();
-            $amount = new Money($data['amount'], new Currency('EUR'));
-            $operation->setAmount($amount);
+            $operation->setAmount($money);
             $operation->setDatetime($data['date']);
             $operation->setDescription('');
             
@@ -87,7 +114,7 @@ class OperationController extends Controller {
             $proceeding->setDate($operation->getDatetime());
             $proceeding->setUser1($data['sender']);
             $proceeding->setUser2($data['receiver']);
-            $proceeding->setAmount($amount);
+            $proceeding->setAmount($money);
             
             $operation->getProceedings()->add($proceeding);
             $em->persist($proceeding);
@@ -112,13 +139,32 @@ class OperationController extends Controller {
      * @Route("/operation/add/{userId}", name="addOperation")
      */
     public function addOperationAction(Request $request, int $userId) {
-        $operation = new Operation();
-        $operation->setDescription('dummy description');
-        $operation->setAmount(Money::EUR(100));
-        $operation->setDatetime(new \DateTime('now'));
+        $me = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $userRepository = $em->getRepository('AppBundle:User');
+        $other = $userRepository->findOneById($userId);
         
+        $data = array(
+                'description' => '',
+                'amount' => '0.00',
+                'datetime' => new \DateTime('now'),
+                'currency' => 'EUR',
+                'users' => array($other),
+                'sender' => $me
+        );
 
-        $builder = $this->createFormBuilder($operation)
+        
+        $friends = $me->getFriends()->toArray();
+        $friendsAndMe = $me->getFriends()->toArray();
+        $friendsAndMe[] = $me;
+        
+        $currencies = array();
+        $isoCurrencies = new ISOCurrencies();
+        foreach ($isoCurrencies as $currency) {
+            $currencies[] = $currency->getCode();
+        }
+        
+        $builder = $this->createFormBuilder($data)
         ->add('description', TextType::class)
         ->add('datetime', DateType::class, array(
                 'widget' => 'single_text',
@@ -127,57 +173,74 @@ class OperationController extends Controller {
                         'class' => 'js-datepicker',
                 )
         ))
-        ->add('amount', TextType::class, array('attr' => array('id' => 'moneyinput')))
-        ->add('users', EntityType::class, array (
-                // query choices from this entity
-                'class' => 'AppBundle:User',
-                'choices' => $this->getUser()->getFriends(),
+        ->add('currency', ChoiceType::class,[
+                'choices' => $currencies,
+                'choice_label' => function($value, $key, $index) {
+                    return $value;
+                },
+                'group_by' => function($value, $key, $index) {
+                    return substr($value, 0, 1);
+                },
+                'attr' => array(
+                    'class' => 'currencyinput'
+                )
+                ])
+        ->add('amount', TextType::class, array('attr' => array('id' => 'moneyinput')
                 
-                // use the User.username property as the visible option string
-                'choice_label' => 'username',
-                
-                // used to render a select box, check boxes or radios
+        ))->add('sender', ChoiceType::class, array (
+                'choices' => $friendsAndMe,
+                'expanded' => false,
+                'choice_label' => function ($value, $key, $index) {
+                    return $value->getUsername();
+                },
+                'multiple' => false,
+                'attr' => array (
+                    'class' => 'js-example-basic-multiple' 
+                )
+        ))->add('users', ChoiceType::class, array (
+                'choices' => $friends,
+                'choice_label' => function ($value, $key, $index) {
+                    return $value->getUsername();
+                },
                 'multiple' => true,
                 'attr' => array (
-                        'width' => '400',
-                        'class' => 'js-example-basic-multiple' 
+                    'class' => 'js-example-basic-multiple' 
                 )
         ))->add('add', SubmitType::class, array (
                 'label' => 'Create Operation' 
         ));
-        
-        
-        $builder->get('amount')
-        ->addModelTransformer(new CallbackTransformer(
-                function ($money) {
-                    // transform the array to a string
-                    return $money->getAmount();
-                },
-                function ($string) {
-                    // transform the string back to an array
-                    return new Money($string, new Currency('EUR'));
-                }
-                ))
-                ;
+
         
         $form = $builder->getForm();
         
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $operationUsers = $operation->getUsers();
-            $operationUsers->add($this->getUser());
+            $data = $form->getData();
+            $operationUsers = $data['users'];
+            $operationUsers[] = $this->getUser();
             $i = 0;
-            $mm = $operation->getAmount()->allocateTo($operation->getUsers()->count());
+            
+            $numberFormatter = new \NumberFormatter('en_US', \NumberFormatter::CURRENCY);
+            $moneyParser = new IntlMoneyParser($numberFormatter, $isoCurrencies);
+            
+            $money = $moneyParser->parse($data['currency'].$data['amount']);
+            
+            $mm = $money->allocateTo(count($operationUsers));
+            $operation = new Operation();
+            $operation->setDescription($data['description']);
+            $operation->setDatetime($data['datetime']);
+            $operation->setAmount($money);
+            
+            $payer = $data['sender'];
             foreach ( $operationUsers as $other ) {
                 $split = new Split();
                 $split->setOperation($operation);
                 $split->setUser($other);
-                if ($other->getId() == $this->getUser()->getId()) {
-                    $split->setPaid($operation->getAmount());
+                if ($other->getId() == $payer->getId()) {
+                    $split->setPaid($money);
                 } else {
-                    $split->setPaid(new Money(0, $operation->getAmount()->getCurrency()));
+                    $split->setPaid(new Money(0, $money->getCurrency()));
                 }
                 $split->setDebt($mm[$i]);
                 $i++;
@@ -186,7 +249,6 @@ class OperationController extends Controller {
                 $em->persist($split);
             }
             
-            $payer = $this->getUser();
             foreach ($operation->getSplits() as $split) {
                 if ($split->getUser()->getId() == $payer->getId()) {
                     continue;
